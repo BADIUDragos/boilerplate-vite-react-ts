b4f072ead237c96c60fe38b0a9a008a3d6429cb4
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-12-05 15:55:33 -0500

    discrepency

--- src/__tests__/store/apis/authApi.test.ts
+++ src/__tests__/store/apis/authApi.test.ts
@@ -33,7 +33,7 @@ describe("Login User", () => {
   const tokenBody: TokensState = {
     access:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJ1c2VyIiwiZW1haWwiOiJ1c2VyQHJvbGxzLXJveWNlLmNvbSIsInBlcm1pc3Npb25zIjpbInZpZXdfY29udGVudCJdLCJpc1N1cGVydXNlciI6ZmFsc2UsImlzU3RhZmYiOmZhbHNlfQ.Xw1uPDh6gBTu34bgsjwAi5F8ahBpbEbMDev33hCoOzE",
-    refresh: "refresh",
+    refresh: "mock_refresh_token",
   };
 
   it("runs the userLoginMutation successfully", async () => {
