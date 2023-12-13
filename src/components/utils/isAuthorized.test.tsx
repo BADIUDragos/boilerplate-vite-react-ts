fee8feb35c14e13d19a1aae71468d22a3328b1fc
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-12-13 08:52:52 -0500

    isAuthorized tests

--- src/components/utils/isAuthorized.test.tsx
+++ src/components/utils/isAuthorized.test.tsx
@@ -16,6 +16,13 @@ describe('isAuthorized', () => {
     expect(result).toBe(true);
   });
 
+  it('should return true for staff when just requiredStaff', () => {
+    const userInfo = createUserInfoState({ isStaff: true})
+
+    const result = isAuthorized(userInfo, [], true);
+    expect(result).toBe(true);
+  });
+
   it('should return true for staff with the required permissions', () => {
     const userInfo = createUserInfoState({permissions: ['view_content'], isStaff: true})
 
@@ -38,9 +45,17 @@ describe('isAuthorized', () => {
   });
 
   it('should return true for normal user with the required permissions', () => {
-    const userInfo = createUserInfoState()
+    const listOfPermissions = ['view_content', 'change_content', 'delete_content']
+    const userInfo = createUserInfoState({permissions: listOfPermissions})
     
-    const result = isAuthorized(userInfo, ['view_content'], false);
+    const result = isAuthorized(userInfo, listOfPermissions, false);
+    expect(result).toBe(true);
+  });
+
+  it('should return false for normal user with partial required permissions', () => {
+    const userInfo = createUserInfoState({permissions: ['view_content', 'change_content']})
+    
+    const result = isAuthorized(userInfo, ['view_content', 'change_content', 'delete_content'], false);
     expect(result).toBe(true);
   });
 
