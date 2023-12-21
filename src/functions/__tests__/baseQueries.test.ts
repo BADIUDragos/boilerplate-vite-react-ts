07f66578f6f3dd713318d1f5daf95c3b5908fa28
BADIUDragos <dragos.badiu.a@hotmail.com>
2023-12-21 14:24:57 +0200

    fixed baseQueryTest

--- src/functions/__tests__/baseQueries.test.ts
+++ src/functions/__tests__/baseQueries.test.ts
@@ -1,29 +1,19 @@
 import { describe, it, expect } from "vitest";
 
-import { getWrapper } from "../../__testUtils__/functions";
 import { useLoginMutation } from "../../store/apis/authApi";
 import { act } from "react-dom/test-utils";
 import { renderHook, waitFor } from "@testing-library/react";
 import { AuthState, LoginCredentials } from "../../store/interfaces/authInterfaces";
-import { createAuthState, createTokensState, createUserInfoState, tokenBody } from "../../store/slices/__tests__/authSetups";
-import { failedMutation, pendingMutation, uninitializedMutation } from "../../store/apis/__tests__/mutationObjectStates";
+import { createAuthState, createTokensState, createUserInfoState, reAuthedTokens, tokenBody } from "../../store/slices/__tests__/authSetups";
 import { initializeTestServer } from "../../__testUtils__/testServerSetup";
 import { baseQueriesHandlers } from "./baseQueriesHandlers";
 import setupStore from "../../store";
+import { getWrapper } from "../../__testUtils__/testStores";
 
-const exampleTokenError = {
-  code: "token_not_valid",
-  messages: [
-    {
-      token_type: "access",
-    },
-  ],
-};
-
-const server = initializeTestServer(baseQueriesHandlers)
+initializeTestServer(baseQueriesHandlers)
 
 describe("baseQueryWithReauth", () => {
-  it("updates tokens on isInvalitTokenError", async () => {
+  it("updates tokens on isInvalidTokenError", async () => {
     const initialAuthState: AuthState = createAuthState({
       userInfo: createUserInfoState(),
       tokens: createTokensState({access: tokenBody.access})
@@ -37,7 +27,6 @@ describe("baseQueryWithReauth", () => {
     });
     
     const [login] = result.current;
-    expect(result.current[1]).toMatchObject(uninitializedMutation);
 
     const userArgs: LoginCredentials = {
       username: "refreshing tokens",
@@ -51,17 +40,11 @@ describe("baseQueryWithReauth", () => {
       login(userArgs);
     });
 
-    expect(result.current[1]).toMatchObject(pendingMutation("login", userArgs));
-
     await waitFor(() => expect(result.current[1].isError).toEqual(true));
-
-    expect(result.current[1]).toMatchObject(
-      failedMutation("login", userArgs)
-    );
 
     const newAuthState = store.getState().auth
 
-    expect(authState.userInfo.permissions).not.toEqual(newAuthState.userInfo.permissions)
+    expect(newAuthState.tokens).toEqual(reAuthedTokens)
 
   });
 });
--- src/functions/__tests__/baseQueriesHandlers.ts
+++ src/functions/__tests__/baseQueriesHandlers.ts
@@ -2,10 +2,21 @@ import { HttpResponse, http } from "msw";
 import { API_URL } from "../../constants/urls";
 import { reAuthedTokens } from "../../store/slices/__tests__/authSetups";
 
+const invalidAccessTokenError = {
+  code: "token_not_valid",
+  messages: [
+    {
+      token_type: "access",
+    },
+  ],
+};
 
 export const baseQueriesHandlers = [
   http.post(`${API_URL}/auth/login`, async () => {
-    return new HttpResponse(null, { status : 401});
+    return new HttpResponse(
+      JSON.stringify(invalidAccessTokenError), 
+      { status: 401 }
+    );
   }),
   http.post(`${API_URL}/auth/login/refresh`, async () => {
     return HttpResponse.json(reAuthedTokens);
